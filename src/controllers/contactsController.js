d9ac341602f993593ab752c76deeedb117540e77
vicentlozano <lozanodroid@gmail.com>
2025-05-07 20:20:36 +0200

    new query

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -3,13 +3,23 @@ const db = require("../config/db");
 
 exports.getContactsById = async (req, res) => {
   if ("userId" in req.query) {
-    const query = ` SELECT *
+    const query = `SELECT u.*
 FROM users u
-WHERE u.id IN (
-    SELECT c.friend_id
-    FROM contacts c
-    where user_id = ${req.query.userId}
-);`;
+JOIN contacts c ON u.id = c.friend_id
+LEFT JOIN (
+    SELECT 
+        CASE 
+            WHEN sendFrom = ${req.query.userId} THEN sendTo
+            ELSE sendFrom
+        END AS contact_id,
+        MAX(date) AS last_message
+    FROM messages
+    WHERE sendFrom = ${req.query.userId} OR sendTo = ${req.query.userId}
+    GROUP BY contact_id
+) m ON u.id = m.contact_id
+WHERE c.user_id = ${req.query.userId}
+ORDER BY m.last_message IS NULL, m.last_message DESC;
+`;
     db.executeQuery(query, (error, result) => {
       if (error) {
         res.status(500).send({
