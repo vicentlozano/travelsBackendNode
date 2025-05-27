9366d8267d4b6f8d64f63b7f07801e0809e73787
vicentlozano <lozanodroid@gmail.com>
2025-05-28 00:14:28 +0200

    delete Image when delete Travel

--- src/controllers/travelsController.js
+++ src/controllers/travelsController.js
@@ -1,15 +1,22 @@
 "use strict";
 const db = require("../config/db");
+const aws = require("../config/aws");
 
 exports.getAllTravels = async (req, res) => {
   const query = `
-  SELECT 
-    t.*, 
-    GROUP_CONCAT(tp.place SEPARATOR ', ') AS places
-  FROM travels t
-  LEFT JOIN travel_places tp ON t.id = tp.travel_id
-  GROUP BY t.id
-`;
+    SELECT 
+      t.id AS travel_id,
+      t.name,
+      t.price,
+      t.travel_date,
+      t.user_id,
+      t.user_name,
+      tp.id AS place_id,
+      tp.place,
+      tp.image
+    FROM travels t
+    LEFT JOIN travel_places tp ON t.id = tp.travel_id
+  `;
   db.executeQuery(query, (error, result) => {
     if (error) {
       res.status(500).send({
@@ -20,9 +27,37 @@ exports.getAllTravels = async (req, res) => {
         },
       });
     } else {
+      // Agrupar los resultados por travel_id
+      const travelsMap = {};
+      result.forEach((row) => {
+        if (!travelsMap[row.travel_id]) {
+          travelsMap[row.travel_id] = {
+            travel_id: row.travel_id,
+            name: row.name,
+            price: row.price,
+            travel_date: row.travel_date.includes("-")
+              ? {
+                  from: row.travel_date.split("-")[0],
+                  to: row.travel_date.split("-")[1],
+                }
+              : row.travel_date,
+            user_id: row.user_id,
+            user_name: row.user_name,
+            places: [],
+          };
+        }
+        if (row.place_id) {
+          travelsMap[row.travel_id].places.push({
+            place_id: row.place_id,
+            place: row.place,
+            image: row.image,
+          });
+        }
+      });
+      const travels = Object.values(travelsMap);
       res.status(200).send({
         error: { status: false, code: 0, source: "" },
-        data: result,
+        data: travels,
       });
     }
   });
@@ -39,7 +74,7 @@ exports.getTravelById = async (req, res) => {
     GROUP BY t.id
   `;
     db.executeQuery(query, (error, result) => {
-      if (error || result.length<1) {
+      if (error || result.length < 1) {
         res.status(500).send({
           error: {
             status: true,
@@ -65,23 +100,51 @@ exports.getTravelById = async (req, res) => {
   }
 };
 exports.deleteTravelById = async (req, res) => {
-  if (req.query.id) {
-    const query = ` delete from travels where id = ${req.query.id}`;
-    db.executeQuery(query, (error, result) => {
-      if (error) {
-        res.status(500).send({
-          error: {
-            status: true,
-            code: 54321,
-            source: "generalError",
-          },
-        });
-      } else {
-        res.status(200).send({
-          error: { status: false, code: 0, source: "" },
+  if (req.query.id && (req.query.urls || req.query["urls[]"])) {
+    let errorFlag = false;
+    try {
+      for (const url of req.query["urls[]"]) {
+        let query = `SELECT COUNT(*) as total FROM travel_places WHERE image = '${url}'`;
+        db.executeQuery(query, async (error, result) => {
+          if (error) {
+           errorFlag = true
+          } else {
+            if (Number(result[0].total) <= 1) {
+              await aws.deleteFileFromS3Url(url);
+            }
+          }
         });
       }
-    });
+    } catch (error) {
+      console.log(error);
+      errorFlag = true;
+    }
+    if (!errorFlag) {
+      const query = ` delete from travels where id = ${req.query.id}`;
+      db.executeQuery(query, (error, result) => {
+        if (error) {
+          res.status(500).send({
+            error: {
+              status: true,
+              code: 54321,
+              source: "generalError",
+            },
+          });
+        } else {
+          res.status(200).send({
+            error: { status: false, code: 0, source: "" },
+          });
+        }
+      });
+    } else {
+      res.status(500).send({
+        error: {
+          status: true,
+          code: 54321,
+          source: "errorDeleteAWS",
+        },
+      });
+    }
   } else {
     res.status(500).send({
       error: {
@@ -93,56 +156,92 @@ exports.deleteTravelById = async (req, res) => {
   }
 };
 exports.createTravel = async (req, res) => {
-  if (req.body.hasOwnProperty("travel")) {
-    let {
-      name,
-      days,
-      price,
-      background_image,
-      travel_date,
-      user_id,
-      user_name,
-      places,
-    } = req.body.travel;
-    const queryInsertTravel = ` insert into travels (name, days, price, background_image, travel_date, user_id, user_name) 
-    values('${name}',${days},${price},'${background_image}','${travel_date}',${user_id},'${user_name}')`;
-    db.executeQuery(queryInsertTravel, (error, result) => {
-      if (error) {
-        res.status(500).send({
-          error: {
-            status: true,
-            code: 54321,
-            source: "generalError",
-          },
-        });
-      } else {
-        let idInserted = result.insertId;
-        let values = "";
-        places.forEach((place, index) => {
-          if (index === places.length - 1) {
-            values += `(${idInserted},'${place}');`;
-          } else {
-            values += `(${idInserted},'${place}'),`;
-          }
-        });
-        let queryInsertPlaces = `insert into travel_places (travel_id,place) values ${values}`;
-        db.executeQuery(queryInsertPlaces, (error, result) => {
-          if (error) {
-            res.status(500).send({
-              error: {
-                status: true,
-                code: 54321,
-                source: "generalError",
-              },
-            });
-          } else {
-            res.status(200).send({
-              error: { status: false, code: 0, source: "" },
-            });
-          }
-        });
-      }
-    });
+  if (
+    Object.hasOwn(req.body, "name") &&
+    req.body.name.trim().length > 0 &&
+    Object.hasOwn(req.body, "price") &&
+    Object.hasOwn(req.body, "travel_date") &&
+    req.body.travel_date.trim().length > 0 &&
+    Object.hasOwn(req.body, "user_id") &&
+    Object.hasOwn(req.body, "user_name") &&
+    req.body.user_name.trim().length > 0 &&
+    Object.hasOwn(req.body, "places") &&
+    req.body.places.length > 0
+  ) {
+    let checkFlag = false;
+    let uploadedUrls = null;
+    try {
+      const uploadPromises = req.files.map((file) => {
+        return aws.uploadBufferToS3(
+          file.buffer,
+          file.originalname,
+          file.mimetype
+        );
+      });
+
+      uploadedUrls = await Promise.all(uploadPromises);
+    } catch (error) {
+      checkFlag = true;
+      console.log(error);
+    }
+    if (!checkFlag) {
+      let name = req.body.name;
+      let price = req.body.price;
+      let travel_date = req.body.travel_date;
+      let user_id = req.body.user_id;
+      let user_name = req.body.user_name;
+      let places = JSON.parse(req.body.places);
+
+      //before insert travel and travel_places we have to upload image's to AWS
+
+      const queryInsertTravel = ` insert into travels (name, price,travel_date, user_id, user_name) 
+    values('${name}',${price},'${travel_date}',${user_id},'${user_name}')`;
+      db.executeQuery(queryInsertTravel, (error, result) => {
+        if (error) {
+          res.status(500).send({
+            error: {
+              status: true,
+              code: 54321,
+              source: "generalError",
+            },
+          });
+        } else {
+          let idInserted = result.insertId;
+          let values = "";
+          places.forEach((place, index) => {
+            if (index === places.length - 1) {
+              values += `(${idInserted},'${place}','${uploadedUrls[index]}');`;
+            } else {
+              values += `(${idInserted},'${place}','${uploadedUrls[index]}'),`;
+            }
+          });
+          let queryInsertPlaces = `insert into travel_places (travel_id,place,image) values ${values}`;
+          db.executeQuery(queryInsertPlaces, (error, result) => {
+            if (error) {
+              res.status(500).send({
+                error: {
+                  status: true,
+                  code: 54321,
+                  source: "generalError",
+                },
+              });
+            } else {
+              res.status(200).send({
+                error: { status: false, code: 0, source: "" },
+              });
+            }
+          });
+        }
+      });
+    } else {
+      res.status(500).send({
+        error: {
+          status: true,
+          code: 54321,
+          source: "UploadError",
+        },
+      });
+    }
   } else {
     res.status(500).send({
       error: {
@@ -154,7 +253,7 @@ exports.createTravel = async (req, res) => {
   }
 };
 exports.updateTravelById = async (req, res) => {
-  console.log(req.body.id)
+  console.log(req.body.id);
 
   if (req.body.id) {
     const id = req.body.id;
--- src/routes/Travels/travelsRoutes.js
+++ src/routes/Travels/travelsRoutes.js
@@ -1,23 +1,24 @@
 "use strict";
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
+
 module.exports = function (app) {
   let travels = require("../../controllers/travelsController");
-  let jwt = require("../../utils/jwtToken")
+  let jwt = require("../../utils/jwtToken");
 
   app
     .route(`/wsTravels/Travels/wsGetAllTravels`)
-    .get(jwt.isValidToken,travels.getAllTravels);
+    .get(jwt.isValidToken, travels.getAllTravels);
   app
     .route(`/wsTravels/Travels/wsDeleteTravelById`)
-    .delete(jwt.isValidToken,travels.deleteTravelById);
-    app
+    .delete(jwt.isValidToken, travels.deleteTravelById);
+  app
     .route(`/wsTravels/Travels/wsCreateTravel`)
-    .post(jwt.isValidToken,travels.createTravel);
-    app
+    .post(jwt.isValidToken, upload.array('images', 5), travels.createTravel);
+  app
     .route(`/wsTravels/Travels/wsUpdateTravelById`)
-    .post(jwt.isValidToken,travels.updateTravelById);
-    app
+    .post(jwt.isValidToken, travels.updateTravelById);
+  app
     .route(`/wsTravels/Travels/wsGetTravelById`)
-    .get(jwt.isValidToken,travels.getTravelById);
-
-}
- 
+    .get(jwt.isValidToken, travels.getTravelById);
+};
