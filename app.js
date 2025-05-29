73e543eda1591a0e093adf41f97284d7f5b51842
vicentlozano <lozanodroid@gmail.com>
2025-05-29 19:53:34 +0200

    fix?

--- app.js
+++ app.js
@@ -21,19 +21,52 @@ const port = 7002;
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-// Add headers
+// // Add headers
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://green-water-0733aec1e.6.azurestaticapps.net"
+//   );
 
-const corsOptions = {
-  origin: [
-    'https://green-water-0733aec1e.6.azurestaticapps.net',
-    'http://localhost:9000'
-  ],
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type,Authorization,langi18n"
+//   );
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
+const allowedOrigins = [
+  'https://green-water-0733aec1e.6.azurestaticapps.net',
+  'http://localhost:9000'
+];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // Permetre peticions sense origen (ex. Postman o curl)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `El CORS no està permès per aquest origen: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'langi18n', 'X-Requested-With']
-};
-
-app.use(cors());
+}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -62,7 +95,7 @@ function checkNodeMinVersion() {
 
   if (parseInt(currentVersion) < requiredVersion) {
     console.error(
-      `⚠️  Error:  Node.js v${requiredVersion} or higher is required. You are currently using v${process.versions.node}.`
+      `⚠️  Error: Node.js v${requiredVersion} or higher is required.You are currently using v${process.versions.node}.`
     );
     process.exit(1);
   }
