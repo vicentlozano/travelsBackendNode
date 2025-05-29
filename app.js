25e14576138d5c378d3aa4860d7da48b9fed6a18
vicentlozano <lozanodroid@gmail.com>
2025-05-29 16:28:33 +0200

    fix

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Add headers
-const cors = require('cors');
 
 const corsOptions = {
   origin: [
